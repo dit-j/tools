954d47cbff2fbd3d9de9c75f33019789acbb623a
dit.k <jawbdit@gmail.com>
2018-04-08 22:50:41 +0200

    Passwort-Länge Bonus immer setzen

--- src/main/java/de/jawb/tools/security/password/PasswordScoreCalculator.java
+++ src/main/java/de/jawb/tools/security/password/PasswordScoreCalculator.java
@@ -13,79 +13,55 @@ import de.jawb.tools.security.password.PasswordAnalysisResult.Penalty;
  * @author dit (15.02.2018)
  */
 public class PasswordScoreCalculator {
-    
-    private static List<String> BLACK_LIST = Arrays.asList(
-            "password", 
-            "123456789",
-            "12345678",
-            "1234567",
-            "123456",
-            "12345",
-            "1234",
-            "123",
-            "qwerty",
-            "abc123",
-            "football",
-            "monkey",
-            "letmein",
-            "111111",
-            "1q2w3e4r",
-            "google",
-            "1q2w3e4r5t",
-            "123qwe",
-            "zxcvbnm",
-            "1q2w3e",
-            "666666",
-            "123321",
-            "suzuki",
-            "yamaha",
-            "honda"
-    );
-    
+
+
+    private static List<String> BLACK_LIST = Arrays.asList("password", "123456789", "12345678", "1234567", "123456", "12345", "1234", "123", "qwerty", "abc123", "football", "monkey", "letmein",
+            "111111", "1q2w3e4r", "google", "1q2w3e4r5t", "123qwe", "zxcvbnm", "1q2w3e", "666666", "123321", "suzuki", "yamaha", "honda");
+
     private PasswordScoreCalculator() {
     }
-    
-    private static String removeBlackListed(String password){
+
+    private static String removeBlackListed(String password) {
         String temp = password;
-        for(String s : BLACK_LIST){
-            if(temp.contains(s)) temp = temp.replaceAll(s, "");
+        for (String s : BLACK_LIST) {
+            if (temp.contains(s)) temp = temp.replaceAll(s, "");
         }
         return temp;
     }
-    
-    public static boolean containsBlackListed(String password){
-        for(String s : BLACK_LIST){
-            if(password.contains(s)) return true;
+
+    public static boolean containsBlackListed(String password) {
+        for (String s : BLACK_LIST) {
+            if (password.contains(s)) return true;
         }
         return false;
     }
-    
+
     public static PasswordAnalysisResult calculateScore(final String password) {
-        
+
         PasswordAnalysisResult result = new PasswordAnalysisResult();
-        
-        String cleaned   = removeBlackListed(password);
-        char[] chars     = cleaned.toCharArray();
+
+        String cleaned = removeBlackListed(password);
+        char[] chars = cleaned.toCharArray();
         int ignoredChars = password.length() - cleaned.length();
-        int countUC      = 0, countLC = 0, countNr = 0;
-        int countSymbols = 0, countRepeatChars = 0; 
+        int countUC = 0, countLC = 0, countNr = 0;
+        int countSymbols = 0, countRepeatChars = 0;
         int countNrOrSymInTheMiddle = 0, countRequirements = 0;
         int countConsecutiveLC = 0;
         int countConsecutiveUC = 0;
         int countConsecutiveNr = 0;
-        
-        final int length            = chars.length;
-        final int minPwLength       = 8; // req1
-        final int minRequirements   = 4;
+
+        final int length = chars.length;
+        final int minPwLength = 8; // req1
+        final int minRequirements = 4;
         final Set<Character> uniqueChars = new HashSet<>();
-        
+
         for (int i = 0; i < length; i++) {
             char ch = chars[i];
-            
+
             uniqueChars.add(ch);
-            
+
             boolean lastChar = i == length - 1;
-            
+
             if (Character.isUpperCase(ch)) { // req2
                 countUC++;
                 if (!lastChar && Character.isUpperCase(chars[i + 1])) {
@@ -111,81 +87,82 @@ public class PasswordScoreCalculator {
                 }
             }
         }
-        
+
         countRepeatChars = length - uniqueChars.size();
 
-        result.setProperty(PasswordProperty.countAllChars,      password.length());
-        result.setProperty(PasswordProperty.countIgnoredChars,  ignoredChars);
-        result.setProperty(PasswordProperty.countRepeatChars,   countRepeatChars);
-        result.setProperty(PasswordProperty.countUC,    countUC);
-        result.setProperty(PasswordProperty.countLC,    countLC);
-        result.setProperty(PasswordProperty.countNr,    countNr);
-        result.setProperty(PasswordProperty.countSym,   countSymbols);
+        result.setProperty(PasswordProperty.countAllChars, password.length());
+        result.setProperty(PasswordProperty.countIgnoredChars, ignoredChars);
+        result.setProperty(PasswordProperty.countRepeatChars, countRepeatChars);
+        result.setProperty(PasswordProperty.countUC, countUC);
+        result.setProperty(PasswordProperty.countLC, countLC);
+        result.setProperty(PasswordProperty.countNr, countNr);
+        result.setProperty(PasswordProperty.countSym, countSymbols);
         result.setProperty(PasswordProperty.countConsecutiveLC, countConsecutiveLC);
         result.setProperty(PasswordProperty.countConsecutiveUC, countConsecutiveUC);
         result.setProperty(PasswordProperty.countConsecutiveNr, countConsecutiveNr);
-        
+
         // Gesamtanzahl
         if (length >= minPwLength) {
             countRequirements++;
-            result.setBonus(Bonus.bonusCharsCount, length * 4);
         }
-        
+
+        result.setBonus(Bonus.bonusCharsCount, length * 4);
+
         // GrossBuchstaben
-        if(countUC > 0){
+        if (countUC > 0) {
             countRequirements++;
             result.setBonus(Bonus.bonusUC, (length - countUC) * 2);
         }
-        
+
         // Kleinbuchstaben
-        if(countLC > 0){
+        if (countLC > 0) {
             countRequirements++;
             result.setBonus(Bonus.bonusLC, (length - countLC) * 2);
         }
-        
+
         // Nummern
-        if(countNr > 0){
+        if (countNr > 0) {
             countRequirements++;
             result.setBonus(Bonus.bonusNr, countNr * 4);
-        } 
-        
+        }
+
         // Sonderzeichen
-        if(countSymbols > 0){
+        if (countSymbols > 0) {
             countRequirements++;
-            if(countUC > 0 || countLC > 0){
+            if (countUC > 0 || countLC > 0) {
                 result.setBonus(Bonus.bonusSymbols, countSymbols * 6);
             }
         }
-        
+
         // Nummer oder Sonderzeichen in der Mitte des Passwortes
-        if(countNrOrSymInTheMiddle > 0){
+        if (countNrOrSymInTheMiddle > 0) {
             result.setBonus(Bonus.bonusSymbolsOrNrInTheMiddle, countNrOrSymInTheMiddle * 2);
         }
-        
+
         // Anforderungen
         if (countRequirements >= minRequirements) {
             result.setBonus(Bonus.bonusMinRequirements, countRequirements * 2);
-        } 
-        
+        }
+
         //
         // ABZUEGE
         //
-        
+
         // Nur Klein- oder Grossbuchstaben
         if ((countLC > 0 || countUC > 0) && countNr == 0 && countSymbols == 0) {
             result.setPenalty(Penalty.penaltyLettersOnly, length);
         }
-        
+
         // Nur Zahlen
         if (countNr > 0 && countLC == 0 && countUC == 0 && countSymbols == 0) {
             result.setPenalty(Penalty.penaltyNumbersOnly, length * 4);
         }
-        
+
         // Mehrere Kleinbuchstaben hintereinander
         if (countConsecutiveLC > 0) {
             result.setPenalty(Penalty.penaltyConsecutiveLC, countConsecutiveLC * 2);
         }
-        
+
         // Mehrere Grossbuchstaben hintereinander
         if (countConsecutiveUC > 0) {
             result.setPenalty(Penalty.penaltyConsecutiveUC, countConsecutiveUC * 2);
@@ -198,12 +175,16 @@ public class PasswordScoreCalculator {
         if (countRepeatChars > 0) {
             result.setPenalty(Penalty.penaltyRepeatChars, countRepeatChars * 2);
         }
-        
+
         return result; //
     }
-    
-    
+
     public static void main(String[] args) {
-        System.out.println(calculateScore("1c23d45/b34567k"));
+        System.out.println(PasswordScoreCalculator.calculateScore("2958344913546871413587993462897"));
+
+//        for(int i = 1; i < 30; i++){
+//            int multiplier = Math.max((i - 4) + 1, 2);
+//            System.out.println(i + ": " + multiplier + " -> "+ (i * multiplier));
+//        }
     }
 }
